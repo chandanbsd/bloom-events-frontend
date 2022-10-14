d6c7615540dddc1e03544581060a8f52b9ad5c8f
Chandan Bangalore Shankar Denanath <chbangal@iu.edu>
2022-10-13 22:54:30 -0400

    refs sprint2-story28-18: User search component and list created

---
+++ src/Mocks/userListMock.js
@@ -0,0 +1,45 @@
+const participantListMock = [
+    {
+        userName: "chandanbsd",
+        firstName: "Chandan",
+        lastName: "Bangalore Shankar Denanath",
+        age: "A18",
+        gender: "Male",
+        isAvailable: "Yes",
+        bio: "Music lover",
+        categoryType: "Music", 
+        categoryLevel: "Beginner",
+        city: "Indianapolis", 
+        state: "Indiana"
+    },
+    {
+        userName: "tombrady",
+        firstName: "Tom",
+        lastName: "Brady",
+        age: "A18",
+        gender: "Male",
+        isAvailable: "Yes",
+        bio: "Professional Football player with multiple super bowl victories. Professional Football player with multiple super bowl victories",
+        categoryType: "Sports", 
+        categoryLevel: "Advanced",
+        city: "Bloomington", 
+        state: "Indiana"
+    },
+    {
+        userName: "jmary",
+        firstName: "Jill",
+        lastName: "Mary",
+        age: "A18",
+        gender: "Female",
+        isAvailable: "No",
+        bio: "Standup comedy with 5+ standup specials",
+        categoryType: "Comedy", 
+        categoryLevel: "Beginner",
+        city: "Nashville", 
+        state: "Tennessee"
+    },
+
+  ];
+  
+  export default participantListMock;
+  
