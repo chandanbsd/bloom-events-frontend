48b813f4bce96df5a0c18eb62cf8a86426ee8883
Chandan Bangalore Shankar Denanath <chbangal@iu.edu>
2022-11-14 04:46:20 -0500

    refs P565P34-72: Venue Creation API complete with enhancement scaffolding

--- src/UserCalendar/UserCalendar.js
+++ src/UserCalendar/UserCalendar.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import baseURL from "../constants/constants";
 import { useDispatch, useSelector } from "react-redux";
-import { setProfile } from "../redux/user";
+import user, { setProfile } from "../redux/user";
 import { clearEvent, setEvent } from "../redux/event";
 import { setActivity } from "../redux/activity";
 import time24 from "../constants/time24";
@@ -97,174 +97,192 @@ const UserCalendar = () => {
         </div>
       </div>
       <div className="d-flex">
-        <div style={{ width: "33%" }}>
-          <div className="mx-auto mt-5 ">
-            <div
-              className="card mx-auto mb-2 p-3 text-center"
-              style={{ minHeight: "300px" }}
-            >
-              <h3 className="mx-auto mt-5" style={{ width: "fit-content" }}>
-                Activity Participation Calendar
-              </h3>
-              <br />
+        {userFromStore.isOwner === "false" ? (
+          <>
+            <div style={{ width: "33%" }} className="mx-auto">
+              <div className="mx-auto mt-5 ">
+                <div
+                  className="card mx-auto mb-2 p-3 text-center"
+                  style={{ minHeight: "300px" }}
+                >
+                  <h3 className="mx-auto mt-5" style={{ width: "fit-content" }}>
+                    Activity Participation Calendar
+                  </h3>
+                  <br />
 
-              {activitiesParticipated !== null ? (
-                <table className="table mx-auto">
-                  <tbody>
-                    <tr>
-                      <th>Activity Name</th>
-                      <th>Time Duration</th>
-                    </tr>
-                    {activitiesParticipated != null &&
-                      activitiesOrganized != null &&
-                      venuesOwned != null &&
-                      activityFromStore.activityList
-                        .filter(
-                          (val) =>
-                            activitiesParticipated.includes(val.activityId) &&
-                            val.activityDate ==
-                              `${date.getUTCFullYear()}-${
-                                date.getUTCMonth() + 1
-                              }-${date.getUTCDate()}`
-                        )
-                        .map((val, i) => (
-                          <tr key={i}>
-                            <td>{val.activityName}</td>
-                            <td>
-                              {`${
-                                time24[Math.min.apply(Math, val.activityTime)]
-                              }` +
-                                "-" +
-                                `${
-                                  time24[Math.max.apply(Math, val.activityTime)]
-                                }`}
-                            </td>
-                          </tr>
-                        ))}
-                  </tbody>
-                </table>
-              ) : (
-                <h1> Loading</h1>
-              )}
-            </div>
-          </div>
-        </div>
-        <div style={{ width: "33%" }}>
-          <div className="mx-auto mt-5 ">
-            <div
-              className="card mx-auto mb-2 p-3 text-center"
-              style={{ minHeight: "300px" }}
-            >
-              <div className="mx-auto">
-                <h3 className="mx-auto mt-5" style={{ width: "fit-content" }}>
-                  Activity Organizer Calendar
-                </h3>
+                  {activitiesParticipated !== null ? (
+                    <table className="table mx-auto">
+                      <tbody>
+                        <tr>
+                          <th>Activity Name</th>
+                          <th>Time Duration</th>
+                        </tr>
+                        {activitiesParticipated != null &&
+                          activitiesOrganized != null &&
+                          venuesOwned != null &&
+                          activityFromStore.activityList
+                            .filter(
+                              (val) =>
+                                activitiesParticipated.includes(
+                                  val.activityId
+                                ) &&
+                                val.activityDate ==
+                                  `${date.getUTCFullYear()}-${
+                                    date.getUTCMonth() + 1
+                                  }-${date.getUTCDate()}`
+                            )
+                            .map((val, i) => (
+                              <tr key={i}>
+                                <td>{val.activityName}</td>
+                                <td>
+                                  {`${
+                                    time24[
+                                      Math.min.apply(Math, val.activityTime)
+                                    ]
+                                  }` +
+                                    "-" +
+                                    `${
+                                      time24[
+                                        Math.max.apply(Math, val.activityTime)
+                                      ]
+                                    }`}
+                                </td>
+                              </tr>
+                            ))}
+                      </tbody>
+                    </table>
+                  ) : (
+                    <h1> Loading</h1>
+                  )}
+                </div>
               </div>
-              <br />
+            </div>
+            <div style={{ width: "33%" }} className="mx-auto">
+              <div className="mx-auto mt-5 ">
+                <div
+                  className="card mx-auto mb-2 p-3 text-center"
+                  style={{ minHeight: "300px" }}
+                >
+                  <div className="mx-auto">
+                    <h3
+                      className="mx-auto mt-5"
+                      style={{ width: "fit-content" }}
+                    >
+                      Activity Organizer Calendar
+                    </h3>
+                  </div>
+                  <br />
 
-              {activitiesParticipated !== null ? (
-                <table className="table mx-auto">
-                  <tbody>
-                    <tr>
-                      <th>Activity Name</th>
-                      <th>Time Duration</th>
-                    </tr>
-                    {activitiesParticipated != null &&
-                      activitiesOrganized != null &&
-                      venuesOwned != null &&
-                      activitiesOrganized
-                        .filter(
-                          (val) =>
-                            val.activityDate ==
+                  {activitiesParticipated !== null ? (
+                    <table className="table mx-auto">
+                      <tbody>
+                        <tr>
+                          <th>Activity Name</th>
+                          <th>Time Duration</th>
+                        </tr>
+                        {activitiesParticipated != null &&
+                          activitiesOrganized != null &&
+                          venuesOwned != null &&
+                          activitiesOrganized
+                            .filter(
+                              (val) =>
+                                val.activityDate ==
+                                `${date.getUTCFullYear()}-${
+                                  date.getUTCMonth() + 1
+                                }-${date.getUTCDate()}`
+                            )
+                            .map((val, i) => (
+                              <tr key={i}>
+                                <td>{val.activityName}</td>
+                                <td>
+                                  {`${
+                                    time24[
+                                      Math.min.apply(Math, val.activityTime)
+                                    ]
+                                  }` +
+                                    "-" +
+                                    `${
+                                      time24[
+                                        Math.max.apply(Math, val.activityTime)
+                                      ]
+                                    }`}
+                                </td>
+                              </tr>
+                            ))}
+                      </tbody>
+                    </table>
+                  ) : (
+                    <h1> Loading</h1>
+                  )}
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <div style={{ width: "33%" }} className="mx-auto">
+            <div className="mx-auto mt-5 ">
+              <div
+                className="card mx-auto mb-2 p-3 text-center"
+                style={{ minHeight: "300px" }}
+              >
+                <h3 className="mx-auto mt-5" style={{ width: "fit-content" }}>
+                  Venue Owner Bookings
+                </h3>
+                <br />
+
+                {activitiesParticipated != null &&
+                activitiesOrganized != null &&
+                venuesOwned != null &&
+                activitiesOrganized ? (
+                  <table className="table mx-auto">
+                    <tbody>
+                      <tr>
+                        <th>Venue Name</th>
+                        <th>Activity Name</th>
+                        <th>Time</th>
+                      </tr>
+                      {activitiesParticipated != null &&
+                        activitiesOrganized != null &&
+                        true &&
+                        venuesOwned !== null &&
+                        venuesOwned.map((val) => {
+                          return val.venueSlots?.[
                             `${date.getUTCFullYear()}-${
                               date.getUTCMonth() + 1
                             }-${date.getUTCDate()}`
-                        )
-                        .map((val, i) => (
-                          <tr key={i}>
-                            <td>{val.activityName}</td>
-                            <td>
-                              {`${
-                                time24[Math.min.apply(Math, val.activityTime)]
-                              }` +
-                                "-" +
-                                `${
-                                  time24[Math.max.apply(Math, val.activityTime)]
-                                }`}
-                            </td>
-                          </tr>
-                        ))}
-                  </tbody>
-                </table>
-              ) : (
-                <h1> Loading</h1>
-              )}
+                          ]?.map(([val1, val2], index) => {
+                            if (val2 != "-1") {
+                              return (
+                                <tr>
+                                  <td>{val.venueName}</td>
+                                  <td>
+                                    {activityFromStore.activityList
+                                      .filter((val) => val.activityId == val2)
+                                      .map((val) => val.activityName)}
+                                  </td>
+                                  <td>{time24[index]}</td>
+                                </tr>
+                              );
+                            }
+                          });
+                        })}
+
+                      <div>
+                        {" "}
+                        {[1, 2, 3].forEach((val) => {
+                          return "adgdgdsfgdfgdfgf";
+                        })}
+                      </div>
+                    </tbody>
+                  </table>
+                ) : (
+                  <h1> Loading</h1>
+                )}
+                {}
+              </div>
             </div>
           </div>
-        </div>
-        <div style={{ width: "33%" }}>
-          <div className="mx-auto mt-5 ">
-            <div
-              className="card mx-auto mb-2 p-3 text-center"
-              style={{ minHeight: "300px" }}
-            >
-              <h3 className="mx-auto mt-5" style={{ width: "fit-content" }}>
-                Venue Owner Calendar
-              </h3>
-              <br />
-
-              {activitiesParticipated != null &&
-              activitiesOrganized != null &&
-              venuesOwned != null &&
-              activitiesOrganized ? (
-                <table className="table mx-auto">
-                  <tbody>
-                    <tr>
-                      <th>Venue Name</th>
-                      <th>Activity Name</th>
-                      <th>Time</th>
-                    </tr>
-                    {activitiesParticipated != null &&
-                      activitiesOrganized != null &&
-                      true &&
-                      venuesOwned !== null &&
-                      venuesOwned.map((val) => {
-                        return val.venueSlots?.[
-                          `${date.getUTCFullYear()}-${
-                            date.getUTCMonth() + 1
-                          }-${date.getUTCDate()}`
-                        ]?.map(([val1, val2], index) => {
-                          if (val2 != "-1") {
-                            return (
-                              <tr>
-                                <td>{val.venueName}</td>
-                                <td>
-                                  {activityFromStore.activityList
-                                    .filter((val) => val.activityId == val2)
-                                    .map((val) => val.activityName)}
-                                </td>
-                                <td>{time24[index]}</td>
-                              </tr>
-                            );
-                          }
-                        });
-                      })}
-
-                    <div>
-                      {" "}
-                      {[1, 2, 3].forEach((val) => {
-                        return "adgdgdsfgdfgdfgf";
-                      })}
-                    </div>
-                  </tbody>
-                </table>
-              ) : (
-                <h1> Loading</h1>
-              )}
-              {}
-            </div>
-          </div>
-        </div>
+        )}
       </div>
     </div>
   );
