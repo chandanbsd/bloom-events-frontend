4b0c47246775045dc9d87972e8fb0b4f786f46eb
Chandan Bangalore Shankar Denanath <denanathchandan5@gmail.com>
2022-11-26 00:19:31 -0500

    backend update

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,5 @@
 // const baseURL = "http://localhost:5000";
-const baseURL = "https://bloomeventssprint4.herokuapp.com/";
+// const baseURL = "https://bloomeventssprint4.herokuapp.com/";
+const baseURL = "https://bloomevents-team34.herokuapp.com/"
 
 export default baseURL;
