f9edd52c423c2f16e4f1c1a7158e7203bec4d0eb
Chandan Bangalore Shankar Denanath <chbangal@iu.edu>
2022-12-01 12:30:21 -0500

    refs sprint5: Stripe API integration for venue payments

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import { useDispatch, useSelector } from "react-redux";
 
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useEffect } from "react";
+import { useEffect, useState, createContext } from "react";
 import PasswordReset from "./PasswordReset/PasswordReset";
 import VenueSearch from "./VenueSearch/VenueSearch";
 import ActivitySearch from "./ActivitySearch/ActivitySearch";
@@ -27,43 +27,78 @@ import UserCalendar from "./UserCalendar/UserCalendar";
 import themeStyles from "./themeStyles";
 import Bookmarks from "./Bookmarks/Bookmarks";
 import VenueCreation from "./VenueCreation/VenueCreation";
-import PaymentStripe from "./PaymentStripe/PaymentStripe";
+
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+import baseURL from "./constants/constants";
+
+import { StripeContext } from "./Context/StripeContext";
 
 function App() {
   const themeFromStore = useSelector((state) => state.theme);
+
+  const stripePromise = loadStripe(
+    "pk_test_51LzNWXKIBfi23JtJ4v9sU7DpP5343HolXzTq8eXI36M95JWa5kK5bbcWqUfOL2O4aUqxHr7uCCFBSF88yvQCHrnp00U4mVBneD"
+  );
+
+  const [clientSecret, setClientSecret] = useState("");
+
+  useEffect(() => {
+    fetch(`${baseURL}/create-payment-intent`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ items: [{ id: "xl-tshirt" }] }),
+    })
+      .then((res) => res.json())
+      .then((data) => setClientSecret(data.clientSecret));
+  }, []);
+
+  const appearance = {
+    theme: "stripe",
+  };
+
+  const options = {
+    clientSecret,
+    appearance,
+  };
+
   return (
     <div className="App">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login/signup" element={<SignUp />} />
-        <Route path="login" element={<Login />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="profile/edit-profile" element={<EditProfile />} />
-        <Route path="login/password-reset/:token" element={<PasswordReset />} />
-        <Route path="venue-search" element={<VenueSearch />} />
-        <Route path="activity-search" element={<ActivitySearch />} />
-        <Route path="participant-search" element={<ParticipantSearch />} />
-        <Route path="login/special-login" element={<SpecialLogin />} />
-        <Route path="login/special-signup" element={<SpecialSignup />} />
-        <Route
-          path="/venue-search/venue-details/:token"
-          element={<VenueDetails />}
-        />
-        <Route
-          path="/venue-search/venue-details/:token/venue-booking"
-          element={<VenueBooking />}
-        />
-        <Route
-          path="/activity-search/activity-details/:token"
-          element={<ActivityDetails />}
-        />
-        <Route path="/calendar" element={<UserCalendar />} />
-        <Route path="/bookmarks" element={<Bookmarks />} />
+      <StripeContext.Provider value={{ options, stripePromise, clientSecret }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="login/signup" element={<SignUp />} />
+          <Route path="login" element={<Login />} />
+          <Route path="profile" element={<Profile />} />
+          <Route path="profile/edit-profile" element={<EditProfile />} />
+          <Route
+            path="login/password-reset/:token"
+            element={<PasswordReset />}
+          />
+          <Route path="venue-search" element={<VenueSearch />} />
+          <Route path="activity-search" element={<ActivitySearch />} />
+          <Route path="participant-search" element={<ParticipantSearch />} />
+          <Route path="login/special-login" element={<SpecialLogin />} />
+          <Route path="login/special-signup" element={<SpecialSignup />} />
+          <Route
+            path="/venue-search/venue-details/:token"
+            element={<VenueDetails />}
+          />
+          <Route
+            path="/venue-search/venue-details/:token/venue-booking"
+            element={<VenueBooking />}
+          />
+          <Route
+            path="/activity-search/activity-details/:token"
+            element={<ActivityDetails />}
+          />
+          <Route path="/calendar" element={<UserCalendar />} />
+          <Route path="/bookmarks" element={<Bookmarks />} />
 
-        <Route path="/venue-creation" element={<VenueCreation />} />
-        <Route path="/payment-stripe" element={<PaymentStripe />} />
-      </Routes>
+          <Route path="/venue-creation" element={<VenueCreation />} />
+        </Routes>
+      </StripeContext.Provider>
     </div>
   );
 }
