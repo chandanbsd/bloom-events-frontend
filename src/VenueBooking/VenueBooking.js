b0bd911812189725f9e58ec8ece0bb24fd9ef323
Chandan Bangalore Shankar Denanath <chbangal@iu.edu>
2022-12-03 06:09:20 -0500

    refs sprint5: Theme styles for venue booking and confirmation pages

--- src/VenueBooking/VenueBooking.js
+++ src/VenueBooking/VenueBooking.js
@@ -8,6 +8,7 @@ import initialTimeSlot from "../constants/initalTimeSlot";
 // import StripeHandler from "../StripePayment/StripeHandler";
 import CheckoutForm from "../StripePayment/CheckoutForm";
 import { Elements } from "@stripe/react-stripe-js";
+import themeStyles from "../themeStyles";
 
 const VenueBooking = () => {
   const location = useLocation();
@@ -97,6 +98,7 @@ const VenueBooking = () => {
   const navigate = useNavigate();
 
   const { clientSecret, options, stripePromise } = useContext(StripeContext);
+  const themeFromStore = useSelector((state) => state.theme);
 
   useEffect(() => {
     if (location.state == undefined) {
@@ -115,56 +117,65 @@ const VenueBooking = () => {
   }, []);
 
   return (
-    <div className="mx-auto mt-5" style={{ width: "50%" }}>
-      {reservationDetails && (
-        <>
-          <div className="card mb-2 p-3">
-            <div className="mx-auto">
-              <h1>Payment Confirmation</h1>
-            </div>
-            <div className="mx-auto justify-content-between">
-              <img
-                src={reservationDetails.activityImage}
-                alt="Activity"
-                style={{ width: "500px" }}
-              />
-              <div>
-                <b>Venue Name:</b> {reservationDetails.venueDetails.venueName}
+    <div className={themeStyles[themeFromStore.value].body}>
+      <div className="mx-auto pt-5" style={{ width: "50%" }}>
+        {reservationDetails && (
+          <>
+            <div
+              className={
+                "card mb-2 p-3 " +
+                themeStyles[themeFromStore.value].bodyHeavy +
+                " " +
+                themeStyles[themeFromStore.value].text
+              }
+            >
+              <div className="mx-auto">
+                <h1>Payment Confirmation</h1>
               </div>
-              <div>
-                <b>Venue Address:</b>{" "}
-                {reservationDetails.venueDetails.venueAddress}
-              </div>
-              <div>
-                <b>Venue Owner:</b> {reservationDetails.venueDetails.venueOwner}
-              </div>
-              <div>
-                <b>Organizer Username:</b> {userFromStore.userName}
-              </div>
-              <div>
-                <b>Selected time slots:</b>{" "}
-                <ul>
-                  {reservationDetails.selectedSlotList.map((val) => (
-                    <li key={val}>{reservationDetails.timeSlots[val]}</li>
-                  ))}
-                </ul>
-              </div>
-              <div>
-                <b>Cost:</b>{" "}
-                {reservationDetails.venueDetails.venueHrCost *
-                  reservationDetails.selectedSlotList.length}
-              </div>
-              <div>
-                {clientSecret && (
-                  <Elements options={options} stripe={stripePromise}>
-                    <CheckoutForm
-                      email={userFromStore.email}
-                      returnUrl={window.location.href}
-                    />
-                  </Elements>
-                )}
-              </div>
-              {/* <button
+              <div className="mx-auto justify-content-between">
+                <img
+                  src={reservationDetails.activityImage}
+                  alt="Activity"
+                  style={{ width: "500px" }}
+                />
+                <div>
+                  <b>Venue Name:</b> {reservationDetails.venueDetails.venueName}
+                </div>
+                <div>
+                  <b>Venue Address:</b>{" "}
+                  {reservationDetails.venueDetails.venueAddress}
+                </div>
+                <div>
+                  <b>Venue Owner:</b>{" "}
+                  {reservationDetails.venueDetails.venueOwner}
+                </div>
+                <div>
+                  <b>Organizer Username:</b> {userFromStore.userName}
+                </div>
+                <div>
+                  <b>Selected time slots:</b>{" "}
+                  <ul>
+                    {reservationDetails.selectedSlotList.map((val) => (
+                      <li key={val}>{reservationDetails.timeSlots[val]}</li>
+                    ))}
+                  </ul>
+                </div>
+                <div>
+                  <b>Cost:</b>{" "}
+                  {reservationDetails.venueDetails.venueHrCost *
+                    reservationDetails.selectedSlotList.length}
+                </div>
+                <div>
+                  {clientSecret && (
+                    <Elements options={options} stripe={stripePromise}>
+                      <CheckoutForm
+                        email={userFromStore.email}
+                        returnUrl={window.location.href}
+                      />
+                    </Elements>
+                  )}
+                </div>
+                {/* <button
                 className="btn btn-success"
                 onClick={() => {
                   handleConfirmation();
@@ -175,31 +186,31 @@ const VenueBooking = () => {
               <button className="btn btn-danger" onClick={() => navigate(-1)}>
                 Cancel Booking
               </button> */}
-              {urlParams.get("redirect_status") === "succeeded" ? (
-                <div className="text-center">
-                  <button
-                    className="btn btn-success"
-                    onClick={() => {
-                      handleConfirmation();
-                    }}
-                  >
-                    Next
+                {urlParams.get("redirect_status") === "succeeded" ? (
+                  <div className="text-center">
+                    <button
+                      className="btn btn-success"
+                      onClick={() => {
+                        handleConfirmation();
+                      }}
+                    >
+                      Next
+                    </button>
+                    <button
+                      className="btn btn-danger"
+                      onClick={() => navigate(-1)}
+                    >
+                      Cancel Booking
+                    </button>
+                  </div>
+                ) : (
+                  <button className="btn btn-warning mx-auto d-block">
+                    Finish Payment to proceed
                   </button>
-                  <button
-                    className="btn btn-danger"
-                    onClick={() => navigate(-1)}
-                  >
-                    Cancel Booking
-                  </button>
-                </div>
-              ) : (
-                <button className="btn btn-warning mx-auto d-block">
-                  Finish Payment to proceed
-                </button>
-              )}
+                )}
+              </div>
             </div>
-          </div>
-          {/* <div>
+            {/* <div>
             <section style={{ width: "500px" }} className="mx-auto mt-5">
               <div className="card-body">
                 <div className="description">
@@ -220,8 +231,9 @@ const VenueBooking = () => {
           </a>
             </section>
           </div> */}
-        </>
-      )}
+          </>
+        )}
+      </div>
     </div>
   );
 };
